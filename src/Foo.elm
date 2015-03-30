a19eff0dae50558a6c2d5c7f510181aa5f279c61
Harry Lachenmayer <harrylachenmayer@gmail.com>
2015-03-30 20:45:53 +0200

    Rebase to get rid of pre-0.15 state.
    movements
    
    add typedefs
    
    upgrade elm-package.json to 0.15
    
    upgrade Foo.elm to 0.15
    
    Tweets
    
    edit tweets

---
+++ src/Foo.elm
@@ -0,0 +1,27 @@
+module Foo where
+
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
+import Text exposing (..)
+
+modulus : number
+modulus =
+  bigger 103 113
+
+encrypt : number -> number -> number
+encrypt m e =
+  m ^ e % modulus
+
+--bigger : number -> number -> number
+bigger first second =if
+    first > second
+  then first
+  else second
+
+main : Element
+main =
+  collage 300 300
+    [ move (-10,0) (filled red (ngon 4 75))
+    , move (50,10) (filled red (ngon 5 50))
+    ]
