36ad538625d950c1e173e4fd20c6321c33989b87
Harry Lachenmayer <harrylachenmayer@gmail.com>
2015-05-26 21:04:15 +0100

    change alias

--- Alias.elm
+++ Alias.elm
@@ -1,11 +1,3 @@
 module Alias where
 
-type alias SomeAlias =
-  { foo : String
-  , bar : List String
-  }
-
-type SomeADT v
-  = Yeah
-  | Nope String Int
-  | DefinitelyNotM8 v String SomeADT
+foo = "bar."
