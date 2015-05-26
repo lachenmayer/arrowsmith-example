4606e0d10f9a0f6cccc2e584cfd535e18f5b66d3
Harry Lachenmayer <harrylachenmayer@gmail.com>
2015-05-26 13:20:40 +0100

    revert stamps

--- src/Stamps.elm
+++ src/Stamps.elm
@@ -4,22 +4,4 @@ import Graphics.Element exposing (..)
 import Graphics.Collage exposing (..)
 import Mouse
 
-main : Signal Element
-main =
-  Signal.map (scene (200, 200)) clickLocations
-
-clickLocations : Signal (List (Int,Int))
-clickLocations =
-  foldp (::) [] (sampleOn Mouse.clicks Mouse.position)
-
-scene : (Int,Int) -> List (Int,Int) -> Element
-scene (w,h) locs =
-  let drawPentagon (x,y) =
-          ngon 5 20
-            |> filled (hsla (toFloat x) 0.9 0.6 0.7)
-            |> move (toFloat x - toFloat w/2, toFloat h/2 - toFloat y)
-            |> rotate (toFloat x)
-  in
-      layers [ collage w h (List.map drawPentagon locs)
-             , plainText "Click to stamp a pentagon."
-             ]
+foo = 32
